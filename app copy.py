267d6100426031d237c35af74ba0302fe8137fa6
Cheolkie Kim <ckiekim@naver.com>
2020-07-20 11:57:29 +0900

    Refactoring

--- app copy.py
+++ app copy.py
@@ -9,7 +9,11 @@ from tensorflow import keras
 from keras.applications.vgg16 import VGG16, decode_predictions
 import numpy as np
 from PIL import Image
-from clu_util import clustering_iris
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 
 app = Flask(__name__)
 
@@ -57,6 +61,49 @@ def nb_transform(review):
 def load_vgg():
     global vgg
     vgg = VGG16()
+
+def clustering_iris(ncls, filename='data/iris.csv'):
+    iris_df = pd.read_csv(filename)
+    del iris_df['Id']
+    target = LabelEncoder().fit_transform(iris_df['Species'])
+    del iris_df['Species']
+    iris_data = iris_df.values
+
+    kmeans = KMeans(n_clusters=ncls, init='k-means++', max_iter=300,
+                    random_state=0)
+    kmeans.fit(iris_data)
+    iris_df['target'] = target
+    iris_df['cluster'] = kmeans.labels_
+
+    pca = PCA(n_components=2)
+    pca_transformed = pca.fit_transform(iris_data)
+    iris_df['pca_x'] = pca_transformed[:,0]
+    iris_df['pca_y'] = pca_transformed[:,1]
+
+    markers=['^', 's', 'o', '*', 'P', 'p']
+    iris_target_name = ['Setosa', 'Versicolor', 'Virginica']
+
+    # PCA 산점도
+    fig, ax = plt.subplots(figsize=(6,4))
+    for i in range(3):
+        x_axis_data = iris_df[iris_df['target']==i]['pca_x']
+        y_axis_data = iris_df[iris_df['target']==i]['pca_y']
+        ax.scatter(x_axis_data, y_axis_data, marker=markers[i],
+                    label=iris_target_name[i])
+    ax.legend()
+    ax.set_xlabel('PCA 1')
+    ax.set_ylabel('PCA 2')
+    fig.savefig('static/images/pca.png')
+
+    # K-Means Clustering 산점도
+    fig, ax = plt.subplots(figsize=(6,4))
+    for i in range(ncls):
+        x_axis_data = iris_df[iris_df['cluster']==i]['pca_x']
+        y_axis_data = iris_df[iris_df['cluster']==i]['pca_y']
+        ax.scatter(x_axis_data, y_axis_data, marker=markers[i])
+    ax.set_xlabel('PCA 1')
+    ax.set_ylabel('PCA 2')
+    fig.savefig('static/images/kmc.png')
 
 @app.route('/')
 def index():
